3ecad56335d3d19a98fac4f7443fce2e09085325
adbutler007 <adam.butler@investresolve.com>
2023-12-30 18:19:21 +0000

    removed timeout wrong arg

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ async def convert_multiple(files: List[UploadFile] = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, timout = 1200)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
